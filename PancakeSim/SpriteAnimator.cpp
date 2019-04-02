17c7ed8df3ab3ac5b0683337d2d108154cc1a456
Ashley Sands <ashsands1@amstrike.com>
2019-04-03 00:17:34 +0100

    inital commit for SpriteAnimator
    dident have enoght time to implerment this today :(

---
+++ PancakeSim/SpriteAnimator.cpp
@@ -0,0 +1,17 @@
+#include "SpriteAnimator.h"
+#include "SpriteSheet.h"
+
+
+SpriteAnimator::SpriteAnimator(SpriteSheet* sprite) : SpriteObject(sprite)
+{
+}
+
+
+SpriteAnimator::~SpriteAnimator()
+{
+}
+
+void SpriteAnimator::Update(Time* time, SDL_Renderer* renderer)
+{
+
+}
--- PancakeSim/SpriteAnimator.h
+++ PancakeSim/SpriteAnimator.h
@@ -1,1 +1,18 @@
+#pragma once
+#include "SpriteObject.h"
+
+class SpriteSheet;
+class Time;
+
 class SpriteAnimator : public SpriteObject
+{
+public:
+	SpriteAnimator(SpriteSheet* sprite);
+	~SpriteAnimator();
+
+	virtual void Update(Time* time, SDL_Renderer* renderer) override;
+
+
+
+};
+
