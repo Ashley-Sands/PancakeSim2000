3d0a7f2cac82011cc5f853850c82e41c7d2034b1
Ashley Sands <ashsands1@amstrike.com>
2019-04-02 22:47:04 +0100

    Added GetRectScaled to Transform

--- PancakeSim/Transform.cpp
+++ PancakeSim/Transform.cpp
@@ -1,13 +1,13 @@
 #include "Transform.h"
 #include "Vector2.h"
-
+#include "SDL.h"
 
 Transform::Transform()
 {
 
 	position = new Vector2();
 	scale = new Vector2();
-
+	rect = new SDL_Rect();
 }
 
 Transform::~Transform()
@@ -15,6 +15,7 @@ Transform::~Transform()
 
 	delete position;
 	delete scale;
+	delete rect;
 
 }
 
@@ -40,4 +41,14 @@ void Transform::SetScale(int x, int y)
 Vector2* Transform::GetScale()
 {
 	return scale;
+}
+
+SDL_Rect* Transform::GetRectScaled(int px_width, int px_height)
+{
+	rect->x = position->x;
+	rect->y = position->y;
+	rect->w = px_width * scale->x;
+	rect->h = px_height * scale->y;
+
+	return rect;
 }
--- PancakeSim/Transform.h
+++ PancakeSim/Transform.h
@@ -1,6 +1,7 @@
 #pragma once
 
 class Vector2;
+class SDL_Rect;
 
 class Transform
 {
@@ -14,9 +15,13 @@ public:
 	void SetScale(int x, int y);
 	Vector2* GetScale();
 
+	SDL_Rect* GetRectScaled(int px_width, int px_height);
+
 private:
 	Vector2* position;
 	Vector2* scale;
+
+	SDL_Rect* rect;
 
 };
 
