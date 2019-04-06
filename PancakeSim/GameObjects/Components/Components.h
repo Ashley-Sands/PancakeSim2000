975bf1e0c58eb4f73e489b1e3c01c5bf43640e47
Ashley Sands <ashsands1@amstrike.com>
2019-04-07 00:41:46 +0100

    Added header files for main game comps and objects

---
+++ PancakeSim/GameObjects/Components/Components.h
@@ -0,0 +1,13 @@
+#pragma once
+
+// Main Includes for all components
+
+#include "Vector2.h"
+#include "Transform.h"
+#include "Time.h"
+
+#include "Sprite.h"
+#include "SpriteSheet.h"
+
+#include "SpriteObject.h"
+#include "SpriteAnimator.h"
---
+++ PancakeSim/GameObjects/GameObjects.h
@@ -0,0 +1,5 @@
+#pragma once
+
+// Main includes for all game objects
+
+#include "FryingPan.h"
