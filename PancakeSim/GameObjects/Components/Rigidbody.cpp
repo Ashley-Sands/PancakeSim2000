0d864208cf70d787b55dcf4272a329ad78e00ead
Ashley Sands <ashsands1@amstrike.com>
2019-04-07 19:23:21 +0100

    Added basic setup to rigidbody :)

--- PancakeSim/GameObjects/Components/Rigidbody.cpp
+++ PancakeSim/GameObjects/Components/Rigidbody.cpp
@@ -1,12 +1,20 @@
 #include "Rigidbody.h"
 
+#include "Time.h"
+#include "Transform.h"
 
-
-Rigidbody::Rigidbody()
+Rigidbody::Rigidbody(Time* gameTime, Transform* attachedTransform)
 {
+	time = gameTime;
+	transform = attachedTransform;
 }
 
 
 Rigidbody::~Rigidbody()
 {
+}
+
+void Rigidbody::Update()
+{
+	// Do physics things
 }
--- PancakeSim/GameObjects/Components/Rigidbody.h
+++ PancakeSim/GameObjects/Components/Rigidbody.h
@@ -1,8 +1,22 @@
 #pragma once
+
+class Time;
+class Transform;
+
 class Rigidbody
 {
 public:
-	Rigidbody();
+	Rigidbody(Time* gameTime, Transform* attachedTransform);
 	~Rigidbody();
+
+	Time* time;
+	Transform* transform;  // the transform that the RB is attached to
+
+	void Update();
+
+
+private:
+	float velocity = 0.0f;
+
 };
 
