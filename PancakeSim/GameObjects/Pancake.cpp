82201f8083382bf1784254b07fa0800597fda255
Ashley Sands <ashsands1@amstrike.com>
2019-04-07 20:55:18 +0100

    corrected values now the velocity is inverted

--- PancakeSim/GameObjects/Pancake.cpp
+++ PancakeSim/GameObjects/Pancake.cpp
@@ -7,20 +7,26 @@
 
 Pancake::Pancake(SpriteSheet* sprite, Time* time) : SpriteAnimator(sprite)
 {
-	rigidbody = new Rigidbody(time, this, new FVector2(0.0f, 1.5f));
+	rigidbody = new Rigidbody(time, this, new FVector2(0.0f, -1.5f));
 }
 
 
 Pancake::~Pancake()
 {
+	delete rigidbody;
 }
 
 void Pancake::Update(float inputValue)
 {
+
+	// Get the current sprite position on the sprite sheet
 	SpriteSheet* spriteSheet = GetSpriteSheet();
 	int spriteID = spriteSheet->GetSpriteIdByPercentage(inputValue);
 
 	spriteSheet->GetSpriteRectByID(spriteID, currentSpritePos);
+
+	//RB bits
+	if( GetPosition()->y >= 200 ) rigidbody->SetVelocity(0.0f, 4.0f);
 
 	rigidbody->Update();
 
