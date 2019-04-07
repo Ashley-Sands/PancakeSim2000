a41e496ad9a5ff54f44eddf66af1e569b4035b41
Ashley Sands <ashsands1@amstrike.com>
2019-04-07 20:20:14 +0100

    add RB to pancake

--- PancakeSim/GameObjects/Pancake.cpp
+++ PancakeSim/GameObjects/Pancake.cpp
@@ -1,9 +1,13 @@
 #include "Pancake.h"
 #include "Components/SpriteSheet.h"
 #include "Components/Sprite.h"
+#include "Components/Rigidbody.h"
+#include "Components/Time.h"
+#include "Components/Vector2.h"
 
-Pancake::Pancake(SpriteSheet* sprite) : SpriteAnimator(sprite)
+Pancake::Pancake(SpriteSheet* sprite, Time* time) : SpriteAnimator(sprite)
 {
+	rigidbody = new Rigidbody(time, this, new FVector2(0.0f, 1.5f));
 }
 
 
@@ -17,5 +21,7 @@ void Pancake::Update(float inputValue)
 	int spriteID = spriteSheet->GetSpriteIdByPercentage(inputValue);
 
 	spriteSheet->GetSpriteRectByID(spriteID, currentSpritePos);
+
+	rigidbody->Update();
 
 }
--- PancakeSim/GameObjects/Pancake.h
+++ PancakeSim/GameObjects/Pancake.h
@@ -1,15 +1,21 @@
 #pragma once
 #include "Components/SpriteAnimator.h"
 
+class Rigidbody;
+class Time;
+
 class Pancake : public SpriteAnimator
 {
 public:
 
-	Pancake(SpriteSheet* sprite);
+	Pancake(SpriteSheet* sprite, Time* time);
 	~Pancake();
 
 	void Update(float inputValue);
 
+
+private:
+	Rigidbody* rigidbody;
 
 };
 
