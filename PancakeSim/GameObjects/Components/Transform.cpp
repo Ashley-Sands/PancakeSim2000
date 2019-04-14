22537a15e9fb2f9509e72454be17daf14aa23b1c
Ashley Sands <ashsands1@amstrike.com>
2019-04-14 23:32:12 +0100

    Added method to set the transform anchor and get the position anchored

--- PancakeSim/GameObjects/Components/Transform.cpp
+++ PancakeSim/GameObjects/Components/Transform.cpp
@@ -1,6 +1,7 @@
 #include "Transform.h"
 #include "Vector2.h"
 #include "SDL.h"
+#include "Debug/Console.h"
 
 Transform::Transform()
 {
@@ -8,6 +9,7 @@ Transform::Transform()
 	position = new FVector2(0, 0);
 	scale = new FVector2(1, 1);
 	rect = new SDL_Rect();
+
 }
 
 Transform::~Transform()
@@ -29,6 +31,39 @@ void Transform::SetPosition(int x, int y)
 FVector2* Transform::GetPosition()
 {
 	return position;
+}
+
+FVector2* Transform::GetAnchoredPosition(int px_width, int px_height, bool ignoreScale /*= false*/)
+{
+
+	if (!ignoreScale)
+	{
+		px_width  *= scale->x;
+		px_height *= scale->y;
+	}
+
+	anchorPosition->x = position->x + (px_width  * anchor->x);
+	anchorPosition->y = position->y + (px_height * anchor->y);
+
+	return anchorPosition;
+
+}
+
+void Transform::SetAnchor(TransformAnchor transfromAnchor)
+{
+	switch (transfromAnchor)
+	{
+	case TransformAnchor::TopLeft:
+		anchor->x = anchor->y = 0;
+		break;
+	case TransformAnchor::Center:
+		anchor->x = anchor->y = 0.5f;
+		break;
+	default:
+		Console::LogMessage(MessageType::Error, " Transfrom Anchor not found");
+		break;
+	}
+
 }
 
 void Transform::SetScale(float x, float y)
--- PancakeSim/GameObjects/Components/Transform.h
+++ PancakeSim/GameObjects/Components/Transform.h
@@ -4,8 +4,15 @@ class Vector2;
 class FVector2;
 class SDL_Rect;
 
+#define out
 //TODO: Add set Position/Scale Anchor
 //      And make Get/Set Posdition take it into account :)
+
+enum TransformAnchor		//TODO: implerment the rest iv only done the ones i need :/
+{
+	TopLeft,
+	Center
+};
 
 class Transform
 {
@@ -15,6 +22,8 @@ public:
 
 	void SetPosition(int x, int y);
 	FVector2* GetPosition();
+	FVector2* GetAnchoredPosition(int px_width, int px_height, bool ignoreScale = false);
+	void SetAnchor(TransformAnchor anchor);
 
 	void SetScale(float x, float y);
 	FVector2* GetScale();
@@ -24,6 +33,9 @@ public:
 private:
 	FVector2* position;
 	FVector2* scale;
+
+	TransformAnchor anchor = TransformAnchor::TopLeft;
+	FVector2* anchorPosition;
 
 	SDL_Rect* rect;
 
