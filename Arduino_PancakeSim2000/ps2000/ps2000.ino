1e8b4a559ca470127edd9423c14a547bd4fc0f7a
Ashley Sands <ashsands1@amstrike.com>
2019-04-15 20:29:05 +0100

    Put MPU device code into its own function and added serial read so we know when to print to console
    iv done it MPU function in a way its can be used to normalized the value and get the current value

--- Arduino_PancakeSim2000/ps2000/ps2000.ino
+++ Arduino_PancakeSim2000/ps2000/ps2000.ino
@@ -4,9 +4,13 @@
 const int MPU_ADDR_0 = 0x68; //AD0 Set to low //TODO: make array
 
 int16_t acc_x, acc_y, acc_z;
+
+int16_t s_gyro_x, s_gyro_y, s_gyro_z;
 int16_t gyro_x, gyro_y, gyro_z;
+
 int16_t temp;
 
+int incomingByte = 0;
 
 char out_str[8];
 
@@ -28,13 +32,34 @@ void setup()
   Wire.write(0x6B);
   Wire.write(0);                        //Set to zero (wake up the MPU-6050)
   Wire.endTransmission(true);
+
+  MPU( &s_gyro_x, &s_gyro_y, &s_gyro_z ); //Get the start values to normalized the device.
+  
 }
 
-void Loop(){
- 
+void loop(){
+
+  MPU( &gyro_x, &gyro_y, &gyro_z );   // get the values from the device.
+
+  //Check that Serial is available and read any incoming bytes
+  if(Serial.available() > 0)
+  {
+    incomingByte = Serial.read();
+
+    if( incomingByte == 'N' )        // Normalize Device 
+      MPU( &s_gyro_x, &s_gyro_y, &s_gyro_z );
+    else if( incomingByte == 'I' )   // Print values (Normalized)
+      Serial.print( convert_int16_to_str(gyro_x - s_gyro_x));
+    else if( incomingByte == 'i' )   // Print values (RAW)
+      Serial.print( convert_int16_to_str(gyro_x));
+  }
+  //print value to console nomalized.
+  
+
+  delay(1000);
 }
 
-void loop() {
+void MPU(int16_t* Gx, int16_t* Gy, int16_t* Gz) {
 
   //Get values from the MPU device.
   Wire.beginTransmission(MPU_ADDR_0);
@@ -43,9 +68,9 @@ void loop() {
   Wire.requestFrom(MPU_ADDR_0, 7*2, true);
 
   // Get values from MPU
-  gyro_x = Wire.read()<<8  | Wire.read(); // 0X3B
-  gyro_y = Wire.read()<<8  | Wire.read(); // 0X3D
-  gyro_z = Wire.read()<<8  | Wire.read(); // 0X3F
+  /*gyro_x*/ *Gx = Wire.read()<<8  | Wire.read(); // 0X3B
+  /*gyro_y*/ *Gy = Wire.read()<<8  | Wire.read(); // 0X3D
+  /*gyro_z*/ *Gz = Wire.read()<<8  | Wire.read(); // 0X3F
 
   temp = Wire.read()<<8   | Wire.read(); // 0X41
 
@@ -53,10 +78,12 @@ void loop() {
   acc_y = Wire.read()<<8 | Wire.read(); // 0X45
   acc_z = Wire.read()<<8 | Wire.read(); // 0X47
 
+  //Serial.print( convert_int16_to_str(gyro_x));
+
   // print some shiz to console :)
-  Serial.print(" Ax: ");   Serial.print( convert_int16_to_str(temp));//gyro_x) );
-  Serial.print(" | Ay: "); Serial.print( convert_int16_to_str(gyro_y) );
-  Serial.print(" | Az: "); Serial.print( convert_int16_to_str(gyro_z) );
-  Serial.print("\n");
-  delay(1000);
+  //Serial.print(" Ax: ");   Serial.print( convert_int16_to_str(temp));//gyro_x) );
+  //Serial.print(" | Ay: "); Serial.print( convert_int16_to_str(gyro_y) );
+  //Serial.print(" | Az: "); Serial.print( convert_int16_to_str(gyro_z) );
+  //Serial.print("\n");
+
 }
