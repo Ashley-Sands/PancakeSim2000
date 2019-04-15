d28bc99a796ba93a9022d08411de9297d46ae97e
Ashley Sands <ashsands1@amstrike.com>
2019-04-16 00:31:43 +0100

    Changed some of the value for fliping pancakes, based on the contrller :)

--- PancakeSim/Game.cpp
+++ PancakeSim/Game.cpp
@@ -200,11 +200,10 @@ void Game::Update()
 	//f++;
 	for (int i = 0; i < panCount; i++)
 	{
-		fryingPans_back[i]->Update(*fryingPans_inputValue[i] / 1023.0f);// (float)(f % 60) / 60.0f);
-		fryingPans_front[i]->Update(*fryingPans_inputValue[i] / 1023.0f);// (float)(f % 60) / 60.0f);
+		fryingPans_back[i]->Update(*fryingPans_inputValue[i] / 800.0f);// (float)(f % 60) / 60.0f);
+		fryingPans_front[i]->Update(*fryingPans_inputValue[i] / 800.0f);// (float)(f % 60) / 60.0f);
 
-		pancakes[i]->Update( fryingPans_inputDelta[i] / 150.0f, fryingPans_back[i]->GetCurrentSpriteId() );// ((float)(f % 60/*(rand() % 60 + 1)*/) / 60.0f) // (float)(f % (int)(60.0f * flipForce)) / (60.0f * flipForce));
-
+		pancakes[i]->Update( fryingPans_inputDelta[i] / 400.0f, fryingPans_back[i]->GetCurrentSpriteId() );// ((float)(f % 60/*(rand() % 60 + 1)*/) / 60.0f) // (float)(f % (int)(60.0f * flipForce)) / (60.0f * flipForce));
 
 
 	}
