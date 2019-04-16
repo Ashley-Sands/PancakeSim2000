7b8ec98666a37ededa41bd9ce200fea20a991b0f
Ashley Sands <ashsands1@amstrike.com>
2019-04-16 03:41:22 +0100

    Added Pancake cooking state
    And semi implermented it

--- PancakeSim/GameObjects/Pancake.cpp
+++ PancakeSim/GameObjects/Pancake.cpp
@@ -9,8 +9,6 @@
 // debuging
 #include "Components/Debug/Console.h"
 
-#define OUT
-
 Pancake::Pancake(SpriteSheet* sprite) : SpriteAnimator(sprite)
 {
 	rigidbody = new Rigidbody(this, 0.2f);
@@ -23,21 +21,22 @@ Pancake::~Pancake()
 	delete rigidbody;
 }
 
-void Pancake::Update(float force, int panSpriteId)
+void Pancake::Update(float force, int panSpriteId)	//TODO: added cooking temp.
 {
 
 	// Get the current sprite position on the sprite sheet, when being fliped
 	SpriteSheet* spriteSheet = GetSpriteSheet();
-	int spriteID = spriteSheet->GetSpriteIdByPercentage(GetFlipPercentage());//inputValue);
+	int spriteID = 0;
 
-	//RB bits
+	
 	//we're in the pan :)
 	//TODO: replace magic numbers throughtout this if statment
 	if (GetPosition()->y >= 365 - (5 * panSpriteId))//TODO: this should really be in the pan bit
 	{
 
-		if (panSpriteId < 4 || force < 0.2f) force = 0.0f;	// can not flip if pan has not roted enought
+		if (panSpriteId < 4 || force < 0.2f || currentCookState == CookingState::Mixture) force = 0.0f;	// can not flip if pan has not roted enought
 		
+		//RB / flip
 		rigidbody->SetVelocity(0.0f, 1.5f * force);
 		currentFlip = 0.0f;
 		currentFlipForce = (flipForce * force);
@@ -49,13 +48,22 @@ void Pancake::Update(float force, int panSpriteId)
 			spriteID = (spriteSheet->GetTotalSprites()-1) - floor(panSpriteId / 2.0f);
 		}
 
+		currentCookingTime += Time::GetDeltaSeconds(); //TODO: Add cooking temp mutiplyer
+		SetCurrentCookingState();
+
 	}
 	else if ( GetPosition()->y >= 310 && rigidbody->GetVelocity()->y < 0)	//correct position when entering pan
 	{
 		spriteID = (spriteSheet->GetTotalSprites() - 1) - floor(panSpriteId / 2.0f);
 	}
+	else // we're fliping pancakes :D
+	{
+		spriteID = spriteSheet->GetSpriteIdByPercentage(GetFlipPercentage());
+	}
 
-	spriteSheet->GetSpriteRectByID(spriteID, OUT currentSpritePos);
+	Console::LogMessage(MessageType::Log, "Pancake Position y: " + std::to_string(GetPosition()->y) +" State: "+ std::to_string(currentCookState) );
+
+	spriteSheet->GetSpriteRectByID(spriteID, /*out*/ currentSpritePos);
 
 	rigidbody->Update();
 
@@ -78,4 +86,24 @@ float Pancake::GetFlipPercentage()
 	}
 
 	return 1.0f - (currentFlip / flipLength);		// invert the flip (sprit sheet is backwards :S )
+}
+
+CookingState Pancake::GetCurrentCookingState()
+{
+	return currentCookState;
+}
+
+void Pancake::SetCurrentCookingState()
+{
+
+	if (currentCookingTime < (cookingLength * mixtureMutiplyer))
+		currentCookState = CookingState::Mixture;
+	else if (currentCookingTime < (cookingLength * rawMutiplyer))
+		currentCookState = CookingState::Raw;
+	else if (currentCookingTime > (cookingLength * fireMutiplyer))
+		currentCookState = CookingState::Fire;
+	else if (currentCookingTime > (cookingLength * burntMutiplyer))
+		currentCookState = CookingState::Burnt;
+	else
+		currentCookState = CookingState::Perfect;
 }
--- PancakeSim/GameObjects/Pancake.h
+++ PancakeSim/GameObjects/Pancake.h
@@ -3,6 +3,15 @@
 
 class Rigidbody;
 
+enum CookingState {
+	None    = 0,
+	Mixture = 1,
+	Raw     = 2,
+	Perfect = 3,
+	Burnt   = 4,
+	Fire    = 5
+};
+
 class Pancake : public SpriteAnimator
 {
 public:
@@ -12,6 +21,7 @@ public:
 
 	void Update(float force, int panSpriteId);
 
+	CookingState GetCurrentCookingState();
 
 private:
 	Rigidbody* rigidbody;
@@ -24,6 +34,19 @@ private:
 	const float flipForce = 4;
 	float currentFlipForce = 0;
 	float counterForce = 2.2f;
+
+	const float cookingLength = 25.0f;		// the perfect cooking leght, in seconds
+	float currentCookingTime = 0.0f;
+
+	CookingState currentCookState = CookingState::None;
+	void SetCurrentCookingState();
+
+	// Pancakes cooking states mutiplyers (cookingLength * mutiplyer)
+	const float mixtureMutiplyer = 0.15f;		// Unflipable, any time below this the pancake is still considered batter.
+	const float rawMutiplyer = 0.75f;			// Unedable, any time below this the pancake is considered raw
+	const float burntMutiplyer = 1.25f;			// Burnt, any time affter this the pancake is considered burnt
+	const float fireMutiplyer = 1.65f;			// any time above this triggers the fire alarm, call the fire bargade!
+
 
 };
 
