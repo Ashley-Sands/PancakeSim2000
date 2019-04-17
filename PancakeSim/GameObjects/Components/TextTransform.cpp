398baa84999d3bad11e4426008a83fc334548788
Ashley Sands <ashsands1@amstrike.com>
2019-04-17 19:37:46 +0100

    Finished adding functionality to TestTransform

--- PancakeSim/GameObjects/Components/TextTransform.cpp
+++ PancakeSim/GameObjects/Components/TextTransform.cpp
@@ -1,10 +1,12 @@
 #include "TextTransform.h"
-
+#include "Debug/Console.h"
 
 
 TextTransform::TextTransform(TTF_Font* font) : Transform()
 {
 	fontFace = font;
+	fontColor = new SDL_Color();
+
 }
 
 
@@ -14,5 +16,58 @@ TextTransform::~TextTransform()
 
 void TextTransform::SetFontFace(TTF_Font* font)
 {
+	if (font == nullptr)
+	{
+		Console::LogMessage(MessageType::Error, "Can not set font face to null");
+		return;
+	}
 	fontFace = font;
+}
+
+void TextTransform::SetFontColor(int r, int g, int b, int a /* = 255 */)
+{
+	fontColor->r = r;
+	fontColor->g = g;
+	fontColor->b = b;
+	fontColor->a = a;
+}
+
+void TextTransform::SetText(std::string str)
+{
+
+	text = str;
+	textHasChanged = true;
+
+}
+
+void TextTransform::Render(SDL_Renderer* renderer)
+{
+	// update the font render texture only if the text has changed
+	if (textHasChanged || fontRenderTexture == nullptr)	
+		UpdateFontTexture(renderer);
+	
+
+	SDL_RenderCopy(renderer, fontRenderTexture, NULL, GetRectScaled(100, 1000));	//TODO: i really need to add size to Transform :(
+
+}
+
+void TextTransform::UpdateFontTexture(SDL_Renderer* renderer)
+{
+
+	//Clear the old fontRenderTexter 
+	if (fontRenderTexture != nullptr)
+		SDL_DestroyTexture(fontRenderTexture);
+
+	// Creat the new texture
+	fontRenderSurface = TTF_RenderText_Solid(fontFace, text.c_str(), *fontColor);
+	fontRenderTexture = SDL_CreateTextureFromSurface(renderer, fontRenderSurface);
+	
+	//Update the transform size //TODO: 
+	//fontRenderSurface->
+
+	// Clean up the old surface.
+	SDL_FreeSurface(fontRenderSurface);
+
+	textHasChanged = false;
+
 }
--- PancakeSim/GameObjects/Components/TextTransform.h
+++ PancakeSim/GameObjects/Components/TextTransform.h
@@ -1,5 +1,8 @@
 #pragma once
 #include "Transform.h"
+#include <string>
+
+#include "SDL.h"
 #include "SDL_ttf.h"
 
 class TextTransform : public Transform
@@ -9,10 +12,27 @@ public:
 	~TextTransform();
 
 	void SetFontFace(TTF_Font* font);
+	void SetFontColor(int r, int g, int b, int a = 255);
+
+	void SetText(std::string text);
+
+	void Render(SDL_Renderer* renderer);
 
 private:
 	TextTransform();
 
 	TTF_Font* fontFace;
+	SDL_Color* fontColor;
+
+	SDL_Surface* fontRenderSurface;
+	SDL_Texture* fontRenderTexture;
+
+	std::string text;
+	bool textHasChanged = false;
+
+	void UpdateFontTexture(SDL_Renderer* renderer);
+
+
+
 };
 
