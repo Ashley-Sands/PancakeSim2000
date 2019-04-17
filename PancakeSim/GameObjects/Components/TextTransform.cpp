02ab5f1e7296dea4c1bb8bde2ab1823fd3afb997
Ashley Sands <ashsands1@amstrike.com>
2019-04-17 17:49:35 +0100

    added constructor and set font

--- PancakeSim/GameObjects/Components/TextTransform.cpp
+++ PancakeSim/GameObjects/Components/TextTransform.cpp
@@ -2,11 +2,17 @@
 
 
 
-TextTransform::TextTransform()
+TextTransform::TextTransform(TTF_Font* font) : Transform()
 {
+	fontFace = font;
 }
 
 
 TextTransform::~TextTransform()
 {
+}
+
+void TextTransform::SetFontFace(TTF_Font* font)
+{
+	fontFace = font;
 }
--- PancakeSim/GameObjects/Components/TextTransform.h
+++ PancakeSim/GameObjects/Components/TextTransform.h
@@ -1,10 +1,18 @@
 #pragma once
 #include "Transform.h"
+#include "SDL_ttf.h"
 
 class TextTransform : public Transform
 {
 public:
-	TextTransform();
+	TextTransform(TTF_Font* font);
 	~TextTransform();
+
+	void SetFontFace(TTF_Font* font);
+
+private:
+	TextTransform();
+
+	TTF_Font* fontFace;
 };
 
