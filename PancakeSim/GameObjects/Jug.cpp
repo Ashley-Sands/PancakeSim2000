3c0e74c656afd13f7380209895b61177a91239fd
Ashley Sands <ashsands1@amstrike.com>
2019-04-20 05:37:13 +0100

    added update and pour functions to jug :D

--- PancakeSim/GameObjects/Jug.cpp
+++ PancakeSim/GameObjects/Jug.cpp
@@ -1,6 +1,6 @@
 #include "Jug.h"
 #include "Components/SpriteSheet.h"
-
+#include "Components/Time.h"
 
 Jug::Jug(SpriteSheet* sprite) : SpriteAnimator(sprite)
 {
@@ -14,5 +14,26 @@ Jug::~Jug()
 void Jug::Begin()
 {
 	SpriteAnimator::Begin();
-	GetSpriteSheet()->GetSpriteRectByID(0, currentSpritePos);
+}
+
+void Jug::Update()
+{
+	// Get the correct sprite for the amount of mixture we have :P
+	int currentSpriteId = GetSpriteSheet()->GetSpriteIdByPercentage( (1.0f-(currentMixture / max_mixture)) );
+	GetSpriteSheet()->GetSpriteRectByID(currentSpriteId, currentSpritePos);
+}
+
+float Jug::Pour()
+{
+
+	if (currentMixture <= 0.0f) return 0;
+
+	float pourRate = maxPourRate * Time::GetDeltaSeconds();
+
+	currentMixture -= pourRate;
+
+	if (currentMixture < 0.0f) currentMixture = 0;
+
+	return pourRate;
+
 }
