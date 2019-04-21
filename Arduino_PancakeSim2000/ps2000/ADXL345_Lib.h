0fdabc6f857b45d47a3f9e277c846db7c68146a1
Ashley Sands <ashsands1@amstrike.com>
2019-04-21 03:22:15 +0100

    add a lib for ADXL

---
+++ Arduino_PancakeSim2000/ps2000/ADXL345_Lib.h
@@ -0,0 +1,30 @@
+#include "Arduino.h"
+#include "Wire.h"
+
+#define ADXL345_ADDR	(0x53)
+ 
+
+class ADXL345{
+
+public:
+
+	ADXL345(TwoWire &w);
+
+	void begin();
+	void Update();
+	
+	int GetGyro_x(){ return gyro_x; }
+	int GetGyro_y(){ return gyro_y; }
+	int GetGyro_z(){ return gyro_z; }
+	
+	
+private: 
+	TwoWire* wire;
+	 
+	int gyro_x = 0;
+	int gyro_y = 0;
+	int gyro_z = 0;
+	
+	byte gyro_values[6];		// we read 6 bytes from the gyro
+	
+};
