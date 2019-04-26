f375aba8f838bfc4443fb1735001a7bac7700448
Ashley Sands <ashsands1@amstrike.com>
2019-04-26 02:41:34 +0100

    inital setup for mainGame Scene

---
+++ PancakeSim/Scenes/Scene_mainGame.cpp
@@ -0,0 +1,28 @@
+#include "Scene_mainGame.h"
+
+
+
+Scene_mainGame::Scene_mainGame(Game* mainGame) : Scene(mainGame)
+{
+}
+
+
+Scene_mainGame::~Scene_mainGame()
+{
+
+}
+
+void Scene_mainGame::Init()
+{
+
+}
+
+void Scene_mainGame::Render()
+{
+
+}
+
+void Scene_mainGame::Update()
+{
+
+}
---
+++ PancakeSim/Scenes/Scene_mainGame.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "Scene.h"
+class Scene_mainGame :	public Scene
+{
+
+public:
+	Scene_mainGame(Game* mainGame);
+	~Scene_mainGame();
+};
+
